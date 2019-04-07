fca5d45a2bcb535ecf51f21378de8cb030e97c92
bwk33 <bk33@students.uwf.edu>
2019-04-06 19:37:16 -0500

    created about us page

---
+++ src/pages/about-us.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const ThirdPage = () => (
+  <div>
+    <div className  = "Hero">
+      <div className = "HeaderTitle">
+      <h1>About Us </h1>
+      <p>Is this article true? How biased is the author? We've all been there.  
+      We read an article online, and don't know whether we should believe it or not.  
+      Suspecto is the answer. If find an article suspiscious, run it through our Chrome plugin and find an answer, instantly.
+      </p>
+      
+        </div>
+      </div>
+    </div>
+)
+
+export default ThirdPage
